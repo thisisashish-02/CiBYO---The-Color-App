f0bdd8639cac74fbf09796648f7b98ec3b35f915
ASHISH <65739459+thisisashish-02@users.noreply.github.com>
2021-06-12 21:15:53 +0530

    add react rotuer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import Palette from './Palette';
+import { Switch, Route } from 'react-router-dom';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Palette palette={generatePalette(seedColors[3])} />
-            </div>
+            <Switch>
+                <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+                <Route
+                    exact
+                    path="/palette/:id"
+                    render={() => <h1>INDIVIDUAL PALETTE</h1>}
+                />
+            </Switch>
+            // <div>
+            //     <Palette palette={generatePalette(seedColors[3])} />
+            // </div>
         )
     }
 }
